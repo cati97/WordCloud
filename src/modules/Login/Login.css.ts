dc2f27f92c48e3bbc5682ef5cdebec21230b9a2c
Katarzyna Siwiec <katasiw97@gmail.com>
2022-04-30 14:41:49 +0200

    center game title in a styled container

---
+++ src/modules/Login/Login.css.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import { Paper } from '@mui/material';
+
+export const Container = styled(Paper)`
+  height: 70vh;
+  margin: 5%;
+  padding: 2%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
---
+++ src/modules/Login/Login.tsx
@@ -0,0 +1,12 @@
+import { Typography } from '@mui/material';
+import * as S from './Login.css';
+
+const Login = () => {
+  return (
+    <S.Container>
+      <Typography variant='h3'>Wordcloud game</Typography>
+    </S.Container>
+  );
+};
+
+export default Login;
