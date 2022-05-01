b4288d076380bf4bbf8dc767fbc54b15fc4535a8
Katarzyna Siwiec <katasiw97@gmail.com>
2022-05-01 11:10:29 +0200

    added mockedQuestions and getting random question function

---
+++ src/modules/Game/Game.css.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import { Typography } from '@mui/material';
+
+export const Question = styled(Typography)`
+  &:first-letter {
+    text-transform: capitalize;
+  }
+`;
--- src/modules/Game/Game.tsx
+++ src/modules/Game/Game.tsx
@@ -1,5 +1,19 @@
+import Container from 'components/Layout/Container';
+import { mockedQuestions } from 'data/mockedQuestions';
+import { useMemo } from 'react';
+import * as S from './Game.css';
+import { getRandomQuestion } from 'utils/getRandomQuestion';
+
 const Game = () => {
-  return <></>;
+  const randomQuestion = useMemo(() => {
+    return getRandomQuestion(mockedQuestions).question;
+  }, []);
+
+  return (
+    <Container>
+      <S.Question variant='h4'> {randomQuestion} </S.Question>
+    </Container>
+  );
 };
 
 export default Game;
