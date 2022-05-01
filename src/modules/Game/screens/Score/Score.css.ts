365669059adf72ae1c57bc56c77e5bff1d8f12c0
Katarzyna Siwiec <katasiw97@gmail.com>
2022-05-01 14:13:55 +0200

    score screen based on logged in user

---
+++ src/modules/Game/screens/Score/Score.css.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const ScoreBox = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  flex-direction: column;
+  height: 30vh;
+`;
--- src/modules/Game/screens/Score/Score.tsx
+++ src/modules/Game/screens/Score/Score.tsx
@@ -1,5 +1,51 @@
-const Score = () => {
-  return <div></div>;
+import { Button, Typography } from '@mui/material';
+import Container from 'components/Layout/Container';
+import { connect } from 'react-redux';
+import { useNavigate } from 'react-router';
+import { GameState } from 'store/store.types';
+import { User } from 'utils/types/User';
+import * as S from './Score.css';
+import * as URL from 'router/url';
+
+interface Props {
+  user: User | null | undefined;
+}
+
+const Score = ({ user }: Props) => {
+  const navigate = useNavigate();
+
+  const navigateHome = () => {
+    navigate(URL.ROOT);
+  };
+
+  return (
+    <Container>
+      {user ? (
+        <S.ScoreBox>
+          <Typography variant='h4'>
+            Congratulations, {user.nickname}!
+          </Typography>
+          <Typography variant='h4'>Your score:</Typography>
+          <Typography color='primary' variant='h4'>
+            {user.score} points
+          </Typography>
+        </S.ScoreBox>
+      ) : (
+        <S.ScoreBox>
+          <Typography variant='h4'>You are not logged in!</Typography>
+          <Button variant='outlined' onClick={navigateHome}>
+            Login
+          </Button>
+        </S.ScoreBox>
+      )}
+    </Container>
+  );
 };
 
-export default Score;
+const mapStateToProps = (state: GameState | undefined) => {
+  return {
+    user: state?.user,
+  };
+};
+
+export default connect(mapStateToProps)(Score);
