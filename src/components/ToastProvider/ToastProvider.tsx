8b2685fb6eda9c3fd716bc4160f731877e0fd2a0
Katarzyna Siwiec <katasiw97@gmail.com>
2022-05-01 10:21:34 +0200

    cleaner code in toast provider

--- src/components/ToastProvider/ToastProvider.tsx
+++ src/components/ToastProvider/ToastProvider.tsx
@@ -1,12 +1,7 @@
 import * as S from './ToastProvider.css';
-import { ReactNode } from 'react';
 
 const ToastProvider = () => {
-  return (
-    <>
-      <S.Toast autoClose={5000} hideProgressBar />
-    </>
-  );
+  return <S.Toast autoClose={5000} hideProgressBar />;
 };
 
 export default ToastProvider;
